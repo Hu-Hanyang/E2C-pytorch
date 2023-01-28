9607bbecbf4a7f4f3a6c976661e13bc307f16bc6
hha160 <HuHY97@outlook.com>
2023-01-27 17:18:06 -0800

    feat: start to design the mpc controller.

--- control_planar.py
+++ control_planar.py
@@ -3,9 +3,10 @@ from e2c_model import E2C
 
 # load the tarned representation model
 model = E2C(obs_dim=1600, z_dim=2, u_dim=2, env='planar') # according to the settings in file train_e2c.py
-model.load_state_dict(torch.load('result/normal_train2/model_5000.pt'))
+model.load_state_dict(torch.load('result/planar/normal_train2/model_5000'))
 
 # set up the controller
+
 
 # step0: random initial one image
 
